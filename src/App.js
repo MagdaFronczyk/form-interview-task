ade04a1d7e9dde15ea778c05c8067b517c61e384
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2018-12-06 18:48:50 +0100

    adds basic form layout

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import React from "react";
 import "./App.css";
 import {withFormik, Form, Field} from "formik";
 import * as Yup from "yup";
+import PropTypes from "prop-types";
 
-const App = ({errors,touched}) => (
-    <Form>
-        <div>
-            {touched.name && errors.name && <p>{errors.name}</p>}
-            <Field type="text" name="name" placeholder="Full Name"/>
-        </div>
-        <div>
-            {touched.email && errors.email && <p>{errors.email}</p>}
-            <Field type="email" name="email" placeholder="Email"/>
-        </div>
-        <div>
-            {touched.password && errors.password && <p>{errors.password}</p>}
-            <Field type="password" name="password" placeholder="Password"/>
-        </div>
-        <button type="submit">Create Account</button>
-    </Form>
+const App = ({
+                 errors,
+                 touched,
+                 isSubmitting
+             }) => (
+    <div className="container">
+        <Form className="form_container">
+            <div className="form">
+                <div id="floatContainer" className="float_container">
+                    <label htmlFor="floatField"/>
+                    {touched.name && errors.name && <p>{errors.name}</p>}
+                    <Field type="text" name="name" placeholder="Full Name" id="floatField"/>
+                </div>
+                <div>
+                    {touched.email && errors.email && <p>{errors.email}</p>}
+                    <Field type="email" name="email" placeholder="Email"/>
+                </div>
+                <div>
+                    {touched.password && errors.password && <p>{errors.password}</p>}
+                    <Field type="password" name="password" placeholder="Password"/>
+                </div>
+            </div>
+            <button type="submit" disabled={isSubmitting}>Create Account</button>
+        </Form>
+    </div>
+
 );
 
 const FormikApp = withFormik({
@@ -26,16 +37,19 @@ const FormikApp = withFormik({
         return {
             name: "",
             email: "",
-            password: ""
+            password: "",
         };
     },
-    handleSubmit(values) {
-        console.log(values);
+    handleSubmit(values, {resetForm}) {
+        setTimeout(() => {
+            console.log(values);
+            resetForm();
+        }, 500);
     },
     validationSchema: Yup.object({
         name: Yup.string("Your name cannot contain numbers").required("Full name is required"),
         email: Yup.string().email("It is not a valid email.").required("Email is required"),
-        password: Yup.string().min(9,"Your password must be 9 characters or longer").required("Password is required."),
+        password: Yup.string().min(8, "Your password must be 9 characters or longer").required("Password is required."),
     })
 })(App);
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FormikApp from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FormikApp />, document.getElementById('root'));

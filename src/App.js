625385f6b04821d2e800166294dfadc321252789
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2018-12-07 11:11:23 +0100

    organizes code and first attemo at showing errors inside the input field

--- src/App.js
+++ src/App.js
@@ -13,22 +13,56 @@ const App = ({
         <Form className="form_container">
             <div className="form">
                 <div className="floating_label">
-                    {touched.name && errors.name && <p>{errors.name}</p>}
-                    <Field type="text" name="name" placeholder="Full Name" className="input"/>
-                    <label htmlFor="name">Full Name</label>
+                    {
+                        touched.name &&
+                        errors.name &&
+                        <p className="error_message">{errors.name}</p>
+                    }
+                    <Field type="text"
+                           name="name"
+                           placeholder="Full Name"
+                           className="input"/>
+                    <label htmlFor="name"
+                           className="label">
+                        Full Name
+                    </label>
                 </div>
                 <div className="floating_label">
-                    {touched.email && errors.email && <p>{errors.email}</p>}
-                    <Field type="email" name="email" placeholder="Email" className="input"/>
-                    <label htmlFor="name">Email</label>
+                    {
+                        touched.email &&
+                        errors.email &&
+                        <p className="error_message">{errors.email}</p>
+                    }
+                    <Field type="email"
+                           name="email"
+                           placeholder="Email"
+                           className="input"/>
+                    <label htmlFor="name"
+                           className="label">
+                        Email
+                    </label>
                 </div>
                 <div className="floating_label">
-                    {touched.password && errors.password && <p>{errors.password}</p>}
-                    <Field type="password" name="password" placeholder="Password" className="input"/>
-                    <label htmlFor="name">Password</label>
+                    {
+                        touched.password &&
+                        errors.password &&
+                        <p className="error_message">{errors.password}</p>
+                    }
+                    <Field type="password"
+                           name="password"
+                           placeholder="Password"
+                           className="input"/>
+                    <label htmlFor="name"
+                           className="label">
+                        Password
+                    </label>
                 </div>
             </div>
-            <button type="submit" disabled={isSubmitting}>Create Account</button>
+            <button type="submit"
+                    disabled={isSubmitting}
+                    className="submit_button">
+                Create Account
+            </button>
         </Form>
     </div>
 
@@ -49,9 +83,17 @@ const FormikApp = withFormik({
         }, 500);
     },
     validationSchema: Yup.object({
-        name: Yup.string("Your name cannot contain numbers").required("Full name is required"),
-        email: Yup.string().email("It is not a valid email.").required("Email is required"),
-        password: Yup.string().min(8, "Your password must be 9 characters or longer").required("Password is required."),
+        name: Yup
+            .string("Your name cannot contain numbers")
+            .required("Full name is required"),
+        email: Yup
+            .string()
+            .email("It is not a valid email.")
+            .required("Email is required"),
+        password: Yup
+            .string()
+            .min(8, "Your password must be 9 characters or longer")
+            .required("Password is required"),
     })
 })(App);
 
